6162c9d14b564292124e36069cc7f221f8d847e3
aeraphe <alberto.aeraph@gmail.com>
2021-01-04 12:14:57 +0000

    Add Comments  on Class FlightController

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -8,8 +8,18 @@ use App\Helpers\Flight123MilhasApiTrait;
 
 class FlightController extends Controller
 {
+    /**
+     * This triat is collection of helper methods
+     * for treat, arrange and group the request flights 
+     * data from 123Milhas Api 
+     */
     use Flight123MilhasApiTrait;
 
+    /**
+     * This Method returns a flights groups arrange by 
+     * descendent Price, Fare and Type (outbound,inbound)
+     * 
+     */
     public function index(FlightHttpService $flightHttpService)
     {
 
@@ -17,7 +27,8 @@ class FlightController extends Controller
         $flightsArrenge = $this->arrangeFlightsByTypeFareAndPrice($flights);
         $flightGroups = $this->makeFlightsGroups($flightsArrenge);
         $flightGroups->flights = $flights;
+        $response = json_encode($flightGroups,JSON_PRETTY_PRINT);
 
-        return json_encode($flightGroups,JSON_PRETTY_PRINT);
+        return response()->json($response);
     }
 }
